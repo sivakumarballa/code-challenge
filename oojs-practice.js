51597663695da27cb5991cc3d61a0304ac781ea5
Siva kumar <sivakumar.balla295@gmail.com>
2017-10-06 08:09:30 +0530

    Custom Group by filter

--- oojs-practice.js
+++ oojs-practice.js
@@ -401,4 +401,23 @@ if(length%2 == 1){
 }
 
 
+@Pipe({name: 'groupBy'})
+export class GroupByPipe implements PipeTransform {
+  transform(value: Array<any>, field: string): Array<any> {
+    const groupedObj = value.reduce((prev, cur)=> {
+      if(!prev[cur[field]]) {
+        prev[cur[field]] = [cur];
+      } else {
+        prev[cur[field]].push(cur);
+      }
+      return prev;
+    }, {});
+    return Object.keys(groupedObj).map(key => ({ key, value: groupedObj[key] }));
+  }
+}
+
+<div *ngFor="let item of employees | groupBy:'department'">
+  Department {{ item.key }} has {{ item.value.length }} employees
+</div>
+
 
